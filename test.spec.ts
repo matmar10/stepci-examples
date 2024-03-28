c064462985693cca88855842f77506f9933a123f
Matthew J. Martin <matthew.mar10@gmail.com>
2024-03-28 15:07:01 +0700

    test: add second workflow using hard-coded env

--- test.spec.ts
+++ test.spec.ts
@@ -1,8 +1,8 @@
 import { runFromFile } from '@stepci/runner';
 import { join } from 'path';
 
-test('Check Response Status', async () => {
-  const { result } = await runFromFile(join(__dirname, 'workflow.yml'));
+test('workflow-1', async () => {
+  const { result } = await runFromFile(join(__dirname, 'workflow-1.yml'));
   if (!result.passed) {
     result.tests.forEach((test) => {
       test.steps.forEach((step, i) => {
@@ -19,3 +19,20 @@ test('Check Response Status', async () => {
   expect(result.passed).toBe(true);
 });
 
+test('workflow-2', async () => {
+  const { result } = await runFromFile(join(__dirname, 'workflow-2.yml'));
+  if (!result.passed) {
+    result.tests.forEach((test) => {
+      test.steps.forEach((step, i) => {
+        if (!step.passed) {
+          console.error(
+            `${test.name || ''} (${test.id}) failed on step #${i}`,
+            step,
+          );
+        }
+        expect(step.passed).toBe(true);
+      });
+    });
+  }
+  expect(result.passed).toBe(true);
+});
